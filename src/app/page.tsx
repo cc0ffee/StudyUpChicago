68102562c4582bf9a57b8a9e852f5e117af8e3b4
Kacper Stasik <stasikkacper1@gmail.com>
2025-06-25 01:24:42 -0500

    feat: mapbox bounded to chicago

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
 "use client"
 import React, { useEffect, useRef } from 'react';
-import Map from 'react-map-gl/mapbox';
+import Map, { NavigationControl } from 'react-map-gl/mapbox';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default function Home() {
+
+  const chicagoBounds = [
+    [-88.0, 41.6], // Southwest
+    [-87.5, 42.1], // Northeast
+  ];
+
   return (
     <Map
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN}
       initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
+        longitude: 41.8781,
+        latitude: -87.6298,
+        zoom: 10,
       }}
-      style={{width: 600, height: 400}}
+      style={{width: '80%', height: 1000}}
       mapStyle="mapbox://styles/mapbox/streets-v11"
+      maxBounds={chicagoBounds}
     />
   );
 }
