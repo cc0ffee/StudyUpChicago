9aaeb6b6dd520b7de00941201e9486df5741bfbe
Kacper Stasik <stasikkacper1@gmail.com>
2025-06-26 17:48:08 -0500

    init: prisma with model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,20 @@ import React, { useEffect, useRef } from 'react';
 import MapComponent from '@/components/map';
 
 export default function Home() {
+  useEffect(() => {
+    fetchPeople();
+  }, []);
+
+  const fetchPeople = async () => {
+    try {
+      const response = await fetch('/api/locations/get');
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching people:', error);
+    }
+  };
+
   return (
     <div className="w-full h-full">
       <MapComponent />
